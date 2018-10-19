1cccface27e4540eddeb8351cec30761730dc0e4
kaede <770705626@qq.com>
2018-10-19 14:13:02 +0800

    Deploy to Heroku

--- src/main/java/com/paas/blog/config/CacheConfiguration.java
+++ src/main/java/com/paas/blog/config/CacheConfiguration.java
@@ -38,6 +38,11 @@ public class CacheConfiguration {
             cm.createCache(com.paas.blog.domain.User.class.getName(), jcacheConfiguration);
             cm.createCache(com.paas.blog.domain.Authority.class.getName(), jcacheConfiguration);
             cm.createCache(com.paas.blog.domain.User.class.getName() + ".authorities", jcacheConfiguration);
+            cm.createCache(com.paas.blog.domain.Blog.class.getName(), jcacheConfiguration);
+            cm.createCache(com.paas.blog.domain.Entry.class.getName(), jcacheConfiguration);
+            cm.createCache(com.paas.blog.domain.Entry.class.getName() + ".tags", jcacheConfiguration);
+            cm.createCache(com.paas.blog.domain.Tag.class.getName(), jcacheConfiguration);
+            cm.createCache(com.paas.blog.domain.Tag.class.getName() + ".entries", jcacheConfiguration);
             // jhipster-needle-ehcache-add-entry
         };
     }
